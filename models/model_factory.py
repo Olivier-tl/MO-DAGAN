666110ff15fc10ae084158c3c9079ad489334df4
Olivier-tl <oliviertl@hotmail.com>
2021-03-21 00:03:04 -0400

    re-re-re-update requirements.txt

--- models/model_factory.py
+++ models/model_factory.py
@@ -8,7 +8,7 @@ from utils import AttrDict
 class ModelFactory:
     def create(model_config: dict) -> torch.nn.Module:
         if model_config['name'] == 'efficientnet':
-            model = EfficientNet.from_name('efficientnet-b8')
+            model = EfficientNet.from_name('efficientnet-b7')
         elif model_config['name'] == 'wgan':
             model = WGAN(AttrDict(model_config['args']))
         else:
