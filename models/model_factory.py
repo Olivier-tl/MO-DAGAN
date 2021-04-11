77eb82c2e3e38113cbb7d7440c0bf20cd563674d
Olivier-tl <oliviertl@hotmail.com>
2021-04-10 23:45:49 -0400

    Runs (#33)
    * save models with dataset name
    
    * code is runnable
    
    * Seems to be working. Test values in config
    
    * Working
    
    Co-authored-by: Nikky Runghen-Vezina <nikky@gamil.com>

--- models/model_factory.py
+++ models/model_factory.py
@@ -8,11 +8,9 @@ from utils import Config
 class ModelFactory:
     def create(model_config: Config.Model) -> torch.nn.Module:
         if model_config.name == 'efficientnet':
-            model = EfficientNetWrapper.from_name('efficientnet-b7',
-                                                  in_channels=model_config.input_channels,
-                                                  image_size=model_config.output_size)
+            model = EfficientNetWrapper.from_name('efficientnet-b7', in_channels=model_config.input_channels)
         elif model_config.name == 'wgan':
-            model = WGAN(channels=model_config.input_channels, img_shape=model_config.output_size)
+            model = WGAN(channels=model_config.input_channels)
         else:
             raise ValueError(f'model name "{model_config.name}" ' f'in config not recognized.')
 
