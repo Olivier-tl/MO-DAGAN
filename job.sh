3ff66d7e80ca35b2c72f568236235f1bbb5472ea
Olivier-tl <oliviertl@hotmail.com>
2021-04-12 21:34:13 -0400

    Working

--- job.sh
+++ job.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #SBATCH --gres=gpu:v100:1
-#SBATCH --time=1:0:0
+#SBATCH --time=0:10:0
 #SBATCH --mem=8G
 #SBATCH --account=def-bengioy
 
-source venv/bin/activate
-cd MO-DAGAN/
+source ../venv/bin/activate
 python run_all.py --task=generation
 python run_all.py --task=classification
