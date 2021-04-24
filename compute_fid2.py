c57900ae42c336fa6ae6c0e868a90676589204de
william <williamneveu@gmail.com>
2021-04-23 20:46:44 -0400

    fid might work

---
+++ compute_fid2.py
@@ -0,0 +1,39 @@
+import subprocess
+
+import numpy as np
+import torch
+from torchvision.utils import save_image
+
+from utils import load_config
+from datasets import DatasetFactory
+from datasets import SyntheticDataset
+
+
+
+config_path = 'configs/classification.yaml'
+dataset_name = 'cifar10'
+imbalance_ratio = 10
+oversampling = 'none'  # none, oversampling, gan
+ada = False
+load_model: bool = False
+
+config_gen = load_config(config_path, dataset_name, imbalance_ratio, oversampling, False, load_model)
+config_gen.dataset.classes = [1]
+sub_ds_size = config_gen.dataset.batch_size
+
+# Instantiating dataloaders
+dataloader_true, _, _ = DatasetFactory.create(dataset_config=config_gen.dataset)
+dataloader_gen = SyntheticDataset(config_gen.dataset)
+
+# Saving DS images to folder
+imgs= next(iter(dataloader_true))
+for i,im in enumerate(imgs[0]):
+    save_image(im, "output/fid_samples_true/im" + str(i) + ".png")
+
+# Saving generated images to folder
+for i in range(sub_ds_size):
+    im = next(iter(dataloader_no_ada))[0]
+    save_image(im, "output/fid_samples_gen/im" + str(i) + ".png")
+
+# Computing FID
+#subprocess.run(["python", "-m", "pytorch_fid", "output/fid_samples_true", "output/fid_samples_gen"])
